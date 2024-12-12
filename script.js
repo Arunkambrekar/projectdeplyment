0a045878997bb61a9511039319de04ba67dd3b7f
Arunkambrekar <97442813+Arunkambrekar@users.noreply.github.com>
2024-12-12 09:30:47 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+let menu = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menu.onlick = () => {
+     menu.classList.toggle('bx-x');
+     navbar.classList.toggle('active');
+}
+
+window.onscroll = () => {
+    menu.classList.remove('bx-x');
+    navbar.classList.remove('active');
+}
+
+// TYPING TEXT CODE
+
+const typed = new Typed('.multiple-text' , {
+    strings: ['Physical Fitness', 'Weight Gain' , 'Strength Training' , 'Fat Lose' , 'Weight Lifting ', 'Running'], typeSpeed: 50,
+    backSpeed: 60  ,
+    backDelay: 1000 ,
+    loop: true ,
+});
